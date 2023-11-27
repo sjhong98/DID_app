b226bb391ed23f760920f40d29acb5553a71e86e
sjhong98 <sjhong98@icloud.com>
2023-11-28 00:36:00 +0900

    feat: qr scan 후 record/vp로 링크 생성 구현

--- views/qr-scan.tsx/QrScan.tsx
+++ views/qr-scan.tsx/QrScan.tsx
@@ -10,20 +10,54 @@ import { RNCamera } from 'react-native-camera';
 
 
 export default function QrScan(): JSX.Element {
+    const [link, setLink] = useState("");
+    const [vpJwt, setVpJwt] = useState("");
+    const [msg, setMsg] = useState("");
+    const [did, setDid] = useState("");
+    const patientJwt = "eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2cCI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVQcmVzZW50YXRpb24iXSwidmVyaWZpYWJsZUNyZWRlbnRpYWwiOlsiZXlKaGJHY2lPaUpGVXpJMU5rc3RVaUlzSW5SNWNDSTZJa3BYVkNKOS5leUoyWXlJNmV5SkFZMjl1ZEdWNGRDSTZXeUpvZEhSd2N6b3ZMM2QzZHk1M015NXZjbWN2TWpBeE9DOWpjbVZrWlc1MGFXRnNjeTkyTVNKZExDSjBlWEJsSWpwYklsWmxjbWxtYVdGaWJHVkRjbVZrWlc1MGFXRnNJbDBzSW1OeVpXUmxiblJwWVd4VGRXSnFaV04wSWpwN0ltbHpjM1ZsY2lJNmV5SnVZVzFsSWpvaVRXVmthV05oYkNCU1pXTnZjbVFnVFdGdVlXZGxiV1Z1ZENCQmMzTnZZMmxoZEdsdmJpSXNJbUZrWkhKbGMzTWlPaUl3ZUROR1pUZEVRalEzTURjeU1EQmxZMFJsTjJRME56ZzRZamd3TldZeU1qVTJSVE5pUXpRNE5qY2lmU3dpZFhObGNrbHVabThpT25zaWJtRnRaU0k2SWlJc0ltVnRZV2xzSWpvaUlpd2lZbWx5ZEdoa1lYa2lPaUlpTENKd2FHOXVaVTUxYldKbGNpSTZJaUlzSW1selJHOWpkRzl5SWpwbVlXeHpaU3dpWVdSa2NtVnpjeUk2SWpCNE5qRmtORGM0TXpBMllXVTFSamsxTm1JMlJXSkdOVFF6TXpVeFJEVTNPRFE1TXpoRU1qRXhSU0o5TENKdFpXUnBZMkZzVW1WamIzSmtjeUk2SWpSbU5UTmpaR0V4T0dNeVltRmhNR013TXpVMFltSTFaamxoTTJWalltVTFaV1F4TW1GaU5HUTRaVEV4WW1FNE56TmpNbVl4TVRFMk1USXdNbUk1TkRVaUxDSmtiMk4wYjNKTWFXTmxibk5sSWpwbVlXeHpaWDE5TENKemRXSWlPbnNpWkdsa0lqb2laR2xrT21WMGFISTZaMjlsY214cE9qQjROakZrTkRjNE16QTJZV1UxUmprMU5tSTJSV0pHTlRRek16VXhSRFUzT0RRNU16aEVNakV4UlNJc0ltRmtaSEpsYzNNaU9pSXdlRFl4WkRRM09ETXdObUZsTlVZNU5UWmlOa1ZpUmpVME16TTFNVVExTnpnME9UTTRSREl4TVVVaWZTd2lhWE56SWpvaVpHbGtPbVYwYUhJNloyOWxjbXhwT2pCNFJUazBRVE0yUmpCRU5ERmpORGhHWkRaa01qUXlSR1kyUXpoRVpEZ3lRekkxTnpVNFlrRTRNU0o5LkdOWjZBVVFvU2RCa2Jsdm1EU2dWYXpMZW5saERqZlgzTGhDRzNpa3BJMTRncnNPZTMteVl5alVreWF4SEFLT1ZlendvN1pEaGNrNjFabmMyV2RxYmxnRSIsImV5SmhiR2NpT2lKRlV6STFOa3N0VWlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKMll5STZleUpBWTI5dWRHVjRkQ0k2V3lKb2RIUndjem92TDNkM2R5NTNNeTV2Y21jdk1qQXhPQzlqY21Wa1pXNTBhV0ZzY3k5Mk1TSmRMQ0owZVhCbElqcGJJbFpsY21sbWFXRmliR1ZEY21Wa1pXNTBhV0ZzSWwwc0ltTnlaV1JsYm5ScFlXeFRkV0pxWldOMElqcDdJbWx6YzNWbGNpSTZleUp1WVcxbElqb2lUV1ZrYVdOaGJDQlNaV052Y21RZ1RXRnVZV2RsYldWdWRDQkJjM052WTJsaGRHbHZiaUlzSW1Ga1pISmxjM01pT2lJd2VETkdaVGRFUWpRM01EY3lNREJsWTBSbE4yUTBOemc0WWpnd05XWXlNalUyUlROaVF6UTROamNpZlN3aWFHOXpjR2wwWVd3aU9pTHNoSnpzbXJqcnM1SHNtNUFpTENKdFpXUnBZMkZzVW1WamIzSmtjeUk2VzExOWZTd2ljM1ZpSWpvaWUxd2laR2xrWENJNlhDSmthV1E2WlhSb2NqcG5iMlZ5YkdrNk1IZ3lRMEl4TnpWQk9UY3lNRE13TmpRelFqaGtNbVl4TmpsRk16VXhaVE01TXpjd01tRTRPRFpoWENJc1hDSmhaR1J5WlhOelhDSTZYQ0l3ZURKRFFqRTNOVUU1TnpJd016QTJORE5DT0dReVpqRTJPVVV6TlRGbE16a3pOekF5WVRnNE5tRmNJbjBpTENKcGMzTWlPaUprYVdRNlpYUm9janBuYjJWeWJHazZNSGc0TXpZeFlURTBNelJFUWpNNVF6STNZakF6TlRreVprSmxObUkzT1RZM01USTBNMkprT1RJNEluMC5vZk1aMm82U1ZvdmhMVlpOZUdrWDN2dEdZTEMzWkpGLTM2UlU1UVAtTkdoUHpJcDM3NHJvY3FnTmg1TmtlMVgtNkxKWmpCN2tjdklDdXVqaTRTWlNwZ0UiXX0sImlzcyI6ImRpZDpldGhyOmdvZXJsaToweGUxM0RiYmMyNDQ5QzM4ODg0Y0ExRTJjYTM5OGRCQzBCZDgwQTk0MjYifQ.pm0nEIipkXZRT7oHxlfro1skuCAtLNg7NHxYREuxdtocbVOFlYl4poSpAKN3qatHVz5wp-vPNqJ3ZHWtKJDakgE"
 
-    const onSuccess = (e:any) => {
-        const test = "test 홍승재 eyJhbGciOiJFUzI1NkstUiIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7Imlzc3VlciI6eyJuYW1lIjoiTWVkaWNhbCBSZWNvcmQgTWFuYWdlbWVudCBBc3NvY2lhdGlvbiIsImFkZHJlc3MiOiIweDNGZTdEQjQ3MDcyMDBlY0RlN2Q0Nzg4YjgwNWYyMjU2RTNiQzQ4NjcifSwiaG9zcGl0YWwiOiLshJzsmrjrs5Hsm5AiLCJtZWRpY2FsUmVjb3JkcyI6W119fSwic3ViIjoie1wiZGlkXCI6XCJkaWQ6ZXRocjpnb2VybGk6MHgyQ0IxNzVBOTcyMDMwNjQzQjhkMmYxNjlFMzUxZTM5MzcwMmE4ODZhXCIsXCJhZGRyZXNzXCI6XCIweDJDQjE3NUE5NzIwMzA2NDNCOGQyZjE2OUUzNTFlMzkzNzAyYTg4NmFcIn0iLCJpc3MiOiJkaWQ6ZXRocjpnb2VybGk6MHg1ZEQ3MDU2MUQ0MjRFNDQxYzYwODIyMDJENkU2NDE2RjdmZkMwMTA5In0._4F0xGeyckEI_sG1uDhL3n2kekpxnQgCTN7lf2jAtQQsYZGbc5rVjtySV4IUubVvv7Qbr9GgL5Lt4Njk3cS75wA"
-        const splittedData = test.split(" ");
-        console.log(splittedData[2]);
+    useEffect(() => {
+        AsyncStorage.getItem("did")
+        .then(res => {
+            setDid(res);
+        })
+    }, []);
+
+    const onSuccess = () => {  
         axios.post('https://api.dmrs.space:5003/link/generate', {
-            payload: splittedData[2]
+            payload: vpJwt
         })
         .then(res => {
-            console.log("===== QRSCAN =====\n", res.data.link);
+            console.log("qr로부터 받아온 link : ", res.data.link);
+            setLink(res.data.link);
+            
+            axios.get(`${res.data.link}`)
+            .then(res => {
+                console.log("link로부터 받아온 vpJwt : ", res.data.payload)
+                setVpJwt(res.data.payload);
+                
+                axios.post('https://api.dmrs.space:5001/user/record/vp', 
+                {
+                    vpJwt: res.data.payload,
+                    did: did
+                })
+                .then(res => {
+                    console.log("===== record/vp =====\n", res);    
+                    setMsg("웹에서 확인하세요");
+                })
+                .catch(err => {
+                    setLink("/record/vp ERROR");
+                    console.log(err);
+                })
+            })
+            setLink(res.data.link);
+            
         })
         .catch(err => {
-            console.log("===== QRSCAN ERROR =====\n", err);
+            setLink("/link/generate ERROR");
+            console.log(err);
         })
+
     }
 
     return (
@@ -33,6 +67,15 @@ export default function QrScan(): JSX.Element {
             </View>
             <View style={styles.contentContainer}>
                 <Text>QrScan</Text>
+                {
+                    msg === "" ?
+                    <QRCodeScanner
+                        onRead={onSuccess}
+                        flashMode={RNCamera.Constants.FlashMode.torch}
+                    />
+                    :
+                    <Text>{msg}</Text>
+                }
                 <QRCodeScanner
                     onRead={onSuccess}
                     flashMode={RNCamera.Constants.FlashMode.torch}
